ef76011c684d926d1654092ade7011e83c9c716b
hujun.chai <hujun.chai@yuemia.com>
2019-11-11 18:17:35 +0800

    Initial commit

---
+++ src/main/java/com/hujun/wechat/web/LoginController.java
@@ -0,0 +1,37 @@
+package com.hujun.wechat.web;
+
+import com.hujun.wechat.util.CheckUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@Controller
+public class LoginController {
+    @RequestMapping(value = "wx",method= RequestMethod.GET)
+    public void login(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("success");
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            if(CheckUtil.checkSignature(signature, timestamp, nonce)){
+                out.write(echostr);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            out.close();
+        }
+
+    }
+}
---
+++ src/test/java/com/hujun/wechat/WechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hujun.wechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
