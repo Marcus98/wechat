3b0ab162e190d8731928aa0ba4665951070ca063
hujun.chai <hujun.chai@yuemia.com>
2019-11-12 16:28:15 +0800

    更新基础信息工具类接口

--- src/main/java/com/hujun/wechat/util/BaseMessageUtil.java
+++ src/main/java/com/hujun/wechat/util/BaseMessageUtil.java
@@ -9,8 +9,6 @@ package com.hujun.wechat.util;
  *
  * 类名称: BaseMessageUtil
  * 类描述: 消息封装工具类的基类,这里采用泛型,方便后期功能扩展
- * @author yuanjun
- * 创建时间:2017年12月6日下午2:06:55
  */
 public interface BaseMessageUtil <T>{
     /**
